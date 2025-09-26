9dc92f55d939b2fbd91728155866ffc59d3d6e98
iqbal899 <jinnatuliqbal99@gmail.com>
2025-09-26 15:08:50 +0530

    Modified

--- app.js
+++ app.js
@@ -15,10 +15,12 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 // --- Connect to MongoDB ---
-mongoose
-  .connect(process.env.MONGO_URL)
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.error("MongoDB connection error:", err));
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log("MongoDB connection error:", err));
 
 // --- Express setup ---
 app.set("view engine", "ejs");
